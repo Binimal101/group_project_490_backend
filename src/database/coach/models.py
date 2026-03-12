bf53dac92467ac0f9c0394600da86eec6917daf8
Rodri09890 <129125043+Rodri09890@users.noreply.github.com>
2026-03-11 21:58:37 -0400

    Update coach models.py

---
+++ src/database/coach/models.py
@@ -0,0 +1,14 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+class Coach(SQLModel, table=True):
+  __tablename__ = "coach"
+  id : int = Field(primary_key=True)
+  verified : bool
+  coach_availability : int = Field(foreign_key="coach_availability.id")
+  last_updated : datetime
+
+class CoachAvailability(SQLModel, table=True):
+  id : int = Field(primary_key=True)
+  is_weekly : bool
+  last_updated : datetime
