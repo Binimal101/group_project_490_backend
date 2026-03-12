75c42a27d0e2ceb04c65b79dc491da365fa8cab6
Rodri09890 <129125043+Rodri09890@users.noreply.github.com>
2026-03-11 22:28:35 -0400

    Update coach models.py

--- src/database/coach/models.py
+++ src/database/coach/models.py
@@ -1,11 +1,12 @@
 from sqlmodel import SQLModel, Field
+from typing import Optional
 from datetime import datetime
 
 class Coach(SQLModel, table=True):
   __tablename__ = "coach"
   id : int = Field(primary_key=True)
   verified : bool
-  coach_availability : int = Field(foreign_key="coach_availability.id")
+  coach_availability : Optional[int] = Field(foreign_key="coach_availability.id")
   last_updated : datetime
 
 class CoachAvailability(SQLModel, table=True):
@@ -14,3 +15,35 @@ class CoachAvailability(SQLModel, table=True):
   is_weekly : bool
   last_updated : datetime
 
+class CoachExperience(SQLModel, table=True):
+  __tablename__ = "coach_experience"
+  id : int = Field(primary_key=True)
+  coach_id : int = Field(foreign_key="coach.id")
+  experience_id : id = Field(foreign_key="experience.id")
+  last_updated : datetime
+
+class Experience(SQLModel, table=True):
+  __tablename__ = "experience"
+  id : int = Field(primary_key=True)
+  experience_name : str
+  experience_title : str
+  experience_description : str
+  experience_start: date
+  experience_end : Optional[date]
+  last_updated : datetime
+
+class CoachCertifications(SQLModel, table=True):
+  __tablename__ = "coach_certifications"
+  id : int = Field(primary_key=True)
+  coach_id = int = Field(foreign_key="coach.id")
+  certification_id : int = Field(foreign_key="certification.id")
+  last_updated : datetime
+
+class Certifications(SQLModel, table=True):
+  __tablename__ = "certifications"
+  id : int = Field(primary_key=True)
+  certification_name : str
+  certification_date : date
+  certification_score : Optional[str]
+  certification_organization: str
+  last_updated : datetime
